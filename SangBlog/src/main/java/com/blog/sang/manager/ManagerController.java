68c7167b3d8048915c712d746c109a40ca5c884a
choisanghyun <itchoi0429@gmail.com>
2019-07-29 21:30:54 +0900

    mysql db 연결 중....

--- SangBlog/src/main/java/com/blog/sang/manager/ManagerController.java
+++ SangBlog/src/main/java/com/blog/sang/manager/ManagerController.java
@@ -2,9 +2,13 @@ package com.blog.sang.manager;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.blog.sang.sequence.SequenceService;
+import com.blog.sang.sequence.domain.Sequence;
 
 @Controller
 @RequestMapping(value="/manager")
@@ -12,8 +16,13 @@ public class ManagerController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ManagerController.class);
 	
+	@Autowired
+	SequenceService sequenceService;
+	
 	@GetMapping("/")
 	public String frontMain() {
+		
+		logger.info("test:: " + sequenceService.getCount("test"));
 		
 		
 		return "/manager/main";
